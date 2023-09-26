0356a921d7be28a23598a80a8fdf6898399ea893
Kate <novak5683@gmail.com>
2023-09-26 19:11:30 +0200

    Updates

---
+++ precache-manifest.ec6696b63458dc3f85c66ea7abea84cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf7e58e4125bf5084fbf",
+    "url": "/got-app/static/css/main.bb0ff220.chunk.css"
+  },
+  {
+    "revision": "bf7e58e4125bf5084fbf",
+    "url": "/got-app/static/js/main.bf7e58e4.chunk.js"
+  },
+  {
+    "revision": "a53a07159f7364041c35",
+    "url": "/got-app/static/css/1.e46789df.chunk.css"
+  },
+  {
+    "revision": "a53a07159f7364041c35",
+    "url": "/got-app/static/js/1.a53a0715.chunk.js"
+  },
+  {
+    "revision": "3585935f3d0611576add",
+    "url": "/got-app/static/js/runtime~main.3585935f.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/got-app/static/media/error.22389cf4.jpeg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/got-app/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "d27547e52cc35141aa051beea540334a",
+    "url": "/got-app/index.html"
+  }
+];
